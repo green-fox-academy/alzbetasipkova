e10c7efdbcb7541cc6477ff6deeaeb0c46221b70
Alzbeta Sipkova <alzbeta.sipkova@gmail.com>
2019-04-07 19:08:41 +0200

    Work from 1st week.

--- Week1/Day4/src/BasicInfo.java
+++ Week1/Day4/src/BasicInfo.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+public class alzbeta {
+    public static void main(String[] args) {
 
-public class BasicInfo {
+        System.out.println("Alžběta");
+        System.out.println("26");
+        System.out.println("1.72");
+    }
 }
--- Week1/Day4/src/CodingHours.java
+++ Week1/Day4/src/CodingHours.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class CodingHours {
+    public static void main (String[] args) {
+
+        int codeHours = 17*5*6;
+        double semesterWorking = 510.0/884.0*100.0;
+
+        System.out.println(codeHours);
+        System.out.println(semesterWorking);
+    }
 }
