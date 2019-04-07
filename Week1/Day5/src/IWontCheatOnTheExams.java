fa5912991a256700bf04c9c6b67b0ba2e51a6a71
Alzbeta Sipkova <alzbeta.sipkova@gmail.com>
2019-04-07 23:17:50 +0200

    Java -  1st week exercises

---
+++ Week1/Day5/src/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+
+        int ch;
+        for (ch = 0; ch < 100 ; ch++) {
+
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
+
+
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
