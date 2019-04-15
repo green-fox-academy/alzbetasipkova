7ee150737b6aa86f7094f08743000db285f6cca2
Alzbeta Sipkova <alzbeta.sipkova@gmail.com>
2019-04-15 19:45:36 +0200

    WriteMultipleLines exercise + my-file2.txt

---
+++ Week3/Day1/src/WriteMultipleLines.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteMultipleLines {
+    public static void main(String[] args) {
+
+   ableToWrite("my-file2.txt", "Martin", 2);
+
+    }
+
+
+    public static void ableToWrite(String path, String word, int number){
+
+
+        Path a = Paths.get(path);
+
+        List<String> contentList = new ArrayList();
+
+        for (int i = 0; i < number; i++) {
+            contentList.add(word);
+        }
+
+        try {
+            Files.write(a, contentList);
+        } catch (IOException er) {
+
+        }
+    }
+}
+
+
+
+// Create a function that takes 3 parameters: a path, a word and a number
+// and is able to write into a file.
+// The path parameter should be a string that describes the location of the file you wish to modify
+// The word parameter should also be a string that will be written to the file as individual lines
+// The number parameter should describe how many lines the file should have.
+// If the word is 'apple' and the number is 5, it should write 5 lines
+// into the file and each line should read 'apple'
+// The function should not raise any errors if it could not write the file.
