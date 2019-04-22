b25596609593a9678ef78cd01a26601752518f22
Alzbeta Sipkova <alzbeta.sipkova@gmail.com>
2019-04-22 17:19:46 +0200

    HOMEWORK

--- HOMEWORK/src/HomeworkBeforeInheritance/Mentor.java
+++ HOMEWORK/src/HomeworkBeforeInheritance/Mentor.java
@@ -1,4 +1,36 @@
 package HomeworkBeforeInheritance;
 
 public class Mentor {
+
+    String name;
+    int age;
+    String gender;
+    String level;
+
+    public Mentor (String name, int age, String gender, String level){
+
+        this.name = name;
+        this.age = age;
+        this. gender = gender;
+        this.level = level;
+
+    }
+
+    public Mentor(){
+
+        name = "Jane Doe";
+        age = 30;
+        gender = "female";
+        level = "intermediate";
+
+    }
+
+    public void introduce(){
+        System.out.println("Hi, I'm " + name + ", " + " a " + age + "year old" + gender );
+    }
+
+    public void getGoal(){
+        System.out.println("My goal is: Live for the moment!");
+    }
+
 }
